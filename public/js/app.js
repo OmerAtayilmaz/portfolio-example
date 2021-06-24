6321a62e947b6e17e85222e96fff36ca38a96d55
Ömer Atayilmaz <64100323+OmerAtayilmaz@users.noreply.github.com>
2021-06-24 12:21:31 +0300

    message

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,15 @@ $(function() {
       $('#sidebar, #content').toggleClass('active');
     });
   });
+
+  document.querySelector('.navigation-nav')
+  .addEventListener('click',(e)=>{
+    e.preventDefault();
+    //Matching strategy
+    if(e.target.classList.contains('nav-link')){
+      const id=e.target.getAttribute('href');
+      console.log("id");
+      document.querySelector(id)
+      .scrollIntoView({behavior:"smooth"});
+    }
+  })
